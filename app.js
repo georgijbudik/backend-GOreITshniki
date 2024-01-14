c29fbb924b617b0a9dcab36c98ca8cca2e773ed1
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2024-01-14 14:36:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use('/exercises', exercisesRouter);
+app.use('/api/exercises', exercisesRouter);
 app.use("/api/users", authRouter);
 
 app.use((req, res) => {
