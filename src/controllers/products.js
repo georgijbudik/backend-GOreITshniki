a3c159575b9c5f28eb42a4f2208db2103b71c00a
georgijbudik <georgijbudik@gmail.com>
2024-01-17 19:16:12 +0100

    finished products-router

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,16 +1,36 @@
 const { ctrlWrapper } = require("../helpers");
 const Product = require("../models/product");
 const ProductCategory = require("../models/productCategory");
+
+const getProducts = async (req, res) => {
+  const { search, category, type } = req.query;
+
+  const { blood } = req.user;
+
+  const productsQuery = Product.find();
+
+  if (search) {
+    productsQuery.where("title").equals(new RegExp(search, "i"));
+  }
+  if (category) {
+    productsQuery.where("category").equals(category);
+  }
+
+  if (type === "recommended") {
+    productsQuery.where("groupBloodNotAllowed." + blood).equals(false);
+  } else if (type === "not recommended") {
+    productsQuery.where("groupBloodNotAllowed." + blood).equals(true);
+  }
+  const products = await Product.find(productsQuery);
+  res.status(200).json(products);
+};
+
 const getProductCategories = async (req, res) => {
   const categories = await ProductCategory.find();
   res.status(200).json(categories);
 };
 
-const getProducts = async (req, res) => {
-  
-};
-
 module.exports = {
-  getProductCategories: ctrlWrapper(getProductCategories),
   getProducts: ctrlWrapper(getProducts),
+  getProductCategories: ctrlWrapper(getProductCategories),
 };
